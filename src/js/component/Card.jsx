59946dc909b79b9e5b649d10e793eb3fec8c3f8b
Roberto González Monge <48779442+beto3103@users.noreply.github.com>
2023-09-16 03:36:26 +0000

    first commit

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+
+const Card = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-3 mb-3">
+                    <div className="card">
+                        <img src="https://picsum.photos/700/400?random" class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card 1</h5>
+                            <p className="card-text">This is the first card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mb-3">
+                    <div className="card">
+                        <img src="https://picsum.photos/700/400?random" class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card 2</h5>
+                            <p className="card-text">This is the second card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mb-3">
+                    <div className="card">
+                        <img src="https://picsum.photos/700/400?random" class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card 3</h5>
+                            <p className="card-text">This is the third card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mb-3">
+                    <img src="https://picsum.photos/700/400?random" class="card-img-top" alt="..." />
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Card 4</h5>
+                            <p className="card-text">This is the second card.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
